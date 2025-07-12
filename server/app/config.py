eb6e761632192d0fc6e2777ebec39f47a3264e05
NityaB24 <nityabalar@gmail.com>
2025-07-13 02:51:16 +0530

    fix(config): env configs

--- server/app/config.py
+++ server/app/config.py
@@ -6,6 +6,4 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_NAME = os.getenv("DB_NAME", "postgres")
 
-DATABASE_URL = (
-    f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
